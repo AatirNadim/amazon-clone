b75be8440ea7e6865cc595d9cfeb3a667c37f561
AatirNadim <aatir.nadim@gmail.com>
2022-09-13 20:45:57 +0530

    working on the payments page

---
+++ src/Payment.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Payment.css";
+function Payment() {
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        {/* payment section : delivery address */}
+        <div className="payment__section">
+          <div className="payment_title"></div>
+        </div>
+        {/* payment section : reviewing the items */}
+        <div className="payment__section"></div>
+        {/* payment section : payment method */}
+        <div className="payment__section"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
